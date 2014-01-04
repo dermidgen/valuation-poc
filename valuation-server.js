c19809a6bdf6fbce44d0dccfd6f6b2c9adce8ab0
Danny Graham <good.midget@gmail.com>
2014-01-03 21:24:57 -0800

    error log calls

--- valuation-server.js
+++ valuation-server.js
@@ -24,7 +24,13 @@ app.get('/propertySearch', function(req, res) {
 	var uri = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id="+zwid+
 			"&address="+req.query.address+"&citystatezip="+req.query.citystatezip;
 
-	console.log(uri);
+	console.log(encodeURIComponent(uri));
+
+	http.get("http://www.google.com/index.html", function(res) {
+		console.log("Got response: " + res.statusCode);
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
 
 	http.get(encodeURIComponent(uri), function(res){
 		console.log(res);
